586995903bf49147b7224dc7b405e98bded04b1b
fengyunan <fengyunan@kuaishou.com>
2021-03-05 11:01:32 +0800

    update

--- smart-ask-community-component/src/test/java/com/feng/community/TestRedis.java
+++ smart-ask-community-component/src/test/java/com/feng/community/TestRedis.java
@@ -2,7 +2,6 @@ package com.feng.community;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
